4002b06c46b28b249d5488918ce70abcfaa6c679
Abhigya Maskay <abhigya.maskay@wiseyak.com>
2024-04-18 05:35:54 -0400

    Chapter 2 exercises

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -7,26 +7,27 @@ require_relative "routing"
 module Rulers
   class Application
     def call(env)
-      if env['PATH_INFO'] == '/favicon.ico'
+      if env["PATH_INFO"] == "/favicon.ico"
         return [404,
-                {'Content-Type' => 'text/html'}, []]
+                { "Content-Type" => "text/html" }, []]
+      elsif env["PATH_INFO"] == "/"
+        return [301, { "Location" => "http://localhost:3001/quotes/a_quote" },
+                []]
       end
 
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
       text = controller.send(act)
-      [200, {'Content-Type' => 'text/html'},
+      [200, { "Content-Type" => "text/html" },
        [text]]
     end
   end
 
   class Controller
     def initialize(env)
-      @env= env
+      @env = env
     end
 
-    def env
-      @env
-    end
+    attr_reader :env
   end
-end 
+end
